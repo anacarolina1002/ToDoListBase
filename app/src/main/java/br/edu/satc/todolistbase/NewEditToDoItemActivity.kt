e0029deb98bdc532e3188bbce8be0361cb6a13ca
Ana Carolina Machado <ana.111811@alunosatc.edu.br>
2024-04-02 21:17:46 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/satc/todolistbase/NewEditToDoItemActivity.kt
@@ -0,0 +1,11 @@
+package br.edu.satc.todolistbase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewEditToDoItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_edit_to_do_item)
+    }
+}
